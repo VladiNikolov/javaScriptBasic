fef30e61b3b469ffc908afff509ab55bbe75799a
Vladimir Nikolov <vladimir.pl.nikolov@gmail.com>
2023-01-21 09:56:23 +0200

    Create transportPrice1.js

---
+++ FirstStepInJavaScript/conditionalStatementsMoreExercise/transportPrice1.js
@@ -0,0 +1,19 @@
+function transportPrice(input){
+    let kilometers = Number(input[0]);
+    let word = input[1];
+    let cheapestTransport = 0 
+
+    if (kilometers < 20){
+        if (word === 'day'){
+            cheapestTransport += kilometers * 0.79 + 0.70;
+        } else if (word == "night"){
+            cheapestTransport += kilometers * 0.90 + 0.70;
+        }
+    } else if (kilometers >= 20 && kilometers < 100){
+        cheapestTransport += kilometers * 0.09;
+    } else if (kilometers >= 100){
+        cheapestTransport += kilometers * 0.06;
+    } 
+    console.log(cheapestTransport.toFixed(2));
+}   
+transportPrice(["180", "night"])  
