57a44e33a27d28529a2b8f1f30050713fd562c04
Vladimir Nikolov <vladimir.pl.nikolov@gmail.com>
2023-01-23 20:03:21 +0200

    Create personalTitles.js

---
+++ FirstStepInJavaScript/conditionalStatementsNestedLab/personalTitles.js
@@ -0,0 +1,16 @@
+function personalTitles(input){
+    let age = Number(input[0]);
+    let gender = input[1];
+
+    if (age >= 16 && gender === "m"){
+        console.log("Mr.");
+    }else if (age < 16 && gender === "m"){
+        console.log("Master");
+    } else if (age >= 16 && gender === "f"){
+        console.log("Ms.");
+    } else if (age < 16 && gender === "f"){
+        console.log("Miss");
+    }
+
+}
+personalTitles(["12", "f"])
