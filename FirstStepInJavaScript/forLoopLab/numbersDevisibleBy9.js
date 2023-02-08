2f48599540cd8708e010125de1e155088a280f5f
Vladimir Nikolov <vladimir.pl.nikolov@gmail.com>
2023-02-08 17:27:25 +0200

    Create numbersDevisibleBy9.js

---
+++ FirstStepInJavaScript/forLoopLab/numbersDevisibleBy9.js
@@ -0,0 +1,19 @@
+function numbersDevisibleBy9(input){
+    let start = Number(input[0]);
+    let end = Number(input[1]);
+    let sum = 0
+
+    for (let i = start; i <= end; i++){
+        if (i % 9 === 0){
+            sum += i
+        }
+    }
+    console.log(`The sum: ${sum}`);
+
+    for (let i = start; i <= end; i++){
+        if (i % 9 === 0){
+            console.log(i);
+        }
+    }
+}
+numbersDevisibleBy9(["100", "200"])
