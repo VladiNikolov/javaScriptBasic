2ca000045bb0b3eca951ebf9d7413d2e35b27e25
Vladimir Nikolov <vladimir.pl.nikolov@gmail.com>
2023-02-20 09:26:26 +0200

    Create clock.js

---
+++ FirstStepInJavaScript/nestedLoopsLab/clock.js
@@ -0,0 +1,10 @@
+
+function clock(input){
+    for (hours = 0; hours <= 23; hours++){
+        for(minutes = 0; minutes <= 59; minutes++){
+            console.log(`${hours}:${minutes}`);
+        }
+    }
+
+} 
+clock()
